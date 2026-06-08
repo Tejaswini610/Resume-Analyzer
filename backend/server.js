f42a614e19d838d59b230a7e1fd8a9b0e0c3416e
Tejaswini610 <pamarthitejaswini@mictech.edu.in>
2026-06-08 20:53:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors());
   });
 
   const storage = multer.memoryStorage();
-const storage = multer.memoryStorage();
+ 
 const upload = multer({
   storage,
   limits: { fileSize: 10 * 1024 * 1024 },
